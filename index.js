3c863f4f839fb7c7fa6dc0faa529cddfb8072de1
iarrieta90 <irati_arrieta90@hotmail.com>
2021-01-26 18:55:24 +0100

    episodes sidebar bug fixed

--- index.js
+++ index.js
@@ -5,10 +5,14 @@ axios
     getEpisodes(),
   ])
   .then((response) => {
-    printEpisodes(response[0].data.results);
-    getEpisodesNext(response[0].data.info.next);
-    // printEpisodeCharacters(response[0].data.results)
-    getEpisodeCharacters(response[0].data.results);
+    var episodesList = [];
+    var nextPage = response[0].data.info.next;
+    episodesList.push(response[0].data.results);
+    // console.log(episodesList[0])
+
+    getEpisodesNext(nextPage, episodesList);
+    printEpisodesList(episodesList[0]);
+    getEpisodeCharacters(episodesList[0]);
   });
 
 // function getCharacters() {
@@ -23,39 +27,33 @@ function getEpisodes() {
   return axios.get(`https://rickandmortyapi.com/api/episode`);
 }
 
-function getEpisodesNext(next) {
-  axios.get(next).then((response) => {
-    $(".sidebar__next").on("click", function () {
-      printEpisodes(response.data.results);
-      if (response.data.info.next !== null) {
-        // getEpisodesNext(response.data.info.next);
-        axios.get(response.data.info.next).then((response) => {
-          printEpisodes(response.data.results);
-          $(".sidebar__next").off("click");
-        });
-      }
-    });
+function getEpisodesNext(nextPage, episodesList) {
+  $(".sidebar__next").on("click", function () {
+    axios
+      .get(nextPage)
+      .then((response) => {
+        nextPage = response.data.info.next;
+        // console.log(nextPage);
+        episodesList.push(response.data.results);
+        const episodesFlat = episodesList.flat();
+        printEpisodesList(episodesFlat);
+        getEpisodeCharacters(episodesFlat);
+      })
+      .catch((err) => {
+        console.log("no more episodes");
+      });
   });
 }
 
 //Takes the result of all episodes data as parameter
-function printEpisodes(episodes) {
+function printEpisodesList(episodes) {
+  $(".sidebar__list__item").remove();
   episodes.forEach((episode) => {
     $(".sidebar__list").append(`
         <li class="sidebar__list__item" data-episode-id="${episode.id}">
             <p class="list__item__title">${episode.name}</p>
             <p>${episode.episode}</p>
         </li>`);
-
-        // $(".sidebar__list").append(`
-        // <li class="sidebar__list__item" data-episode-id="${episode.id}">
-        //     <p class="list__item__title">${episode.name}</p>
-        //     <div class="list__item__div">
-        //         <p>${episode.air_date}</p>
-        //         <p>${episode.episode}</p>
-        //     </div>
-        // </li>`);
-
   });
 }
 
@@ -70,30 +68,18 @@ function getEpisodeCharacters(episodes) {
     $(".container_cards").remove();
     $(".container_main").append(containerCards);
 
-    
-
+    console.log(episodes);
 
     //Iterate over the episodes array, and for each episode compare its id with the target id
     episodes.forEach((episode) => {
       if (episode.id == episodeId) {
-
-        $(".episode_preview").remove();
-        $(".container_episode").append(`
-          <div class="episode_preview">
-            <h1>${episode.name}</h1>
-            <div>
-              <p>${episode.air_date}</p>
-              <p>${episode.episode}</p>
-            </div> 
-          </div>
-        `)
+        printEpisodeInfo(episode);
 
         //If the ids match, iterate over that episode's characters array
         //and make an API call for each character
         episode.characters.forEach((character) => {
           axios.get(character).then((response) => {
             printEpisodeCharacters(response.data);
-            
           });
         });
       }
@@ -103,7 +89,6 @@ function getEpisodeCharacters(episodes) {
 
 //Take the data of each character's individual API call as parameter
 function printEpisodeCharacters(character) {
-  console.log(character.name);
   $(".container_cards").append(`
   <div class="character_card__container">
     <div class="character_card">
@@ -124,11 +109,25 @@ function printEpisodeCharacters(character) {
   `);
 }
 
-$(document).ready(function () {
-  const containerMain = $(`<div class="container_main"></div>`)
-  const episodeInfo = $(`<div class="container_episode"></div>`);
-  $('main').append(containerMain);
-  $(containerMain).append(episodeInfo);
-  $(episodeInfo).append(`<h1 class="episode_preview">Select an episode to show its characters</h1>`)
+function printEpisodeInfo(episode) {
+  $(".episode_preview").remove();
+  $(".container_episode").append(`
+    <div class="episode_preview">
+      <h1>${episode.name}</h1>
+      <div>
+        <p>${episode.air_date}</p>
+        <p>${episode.episode}</p>
+      </div> 
+    </div>
+  `);
+}
 
+$(document).ready(function () {
+  const containerMain = $(`<div class="container_main"></div>`);
+  const episodeInfo = $(`<div class="container_episode"></div>`);
+  $("main").append(containerMain);
+  $(containerMain).append(episodeInfo);
+  $(episodeInfo).append(
+    `<h1 class="episode_preview">Select an episode to show its characters</h1>`
+  );
 });
