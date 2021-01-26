c6e9c22cd57ae05ecfe12f4501e8432e57cd2665
iarrieta90 <irati_arrieta90@hotmail.com>
2021-01-26 17:42:46 +0100

    characters basic info ok

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ axios
     printEpisodes(response[0].data.results);
     getEpisodesNext(response[0].data.info.next);
     // printEpisodeCharacters(response[0].data.results)
-    getEpisodeCharacters(response[0].data.results)
+    getEpisodeCharacters(response[0].data.results);
   });
 
 // function getCharacters() {
@@ -34,7 +34,6 @@ function getEpisodesNext(next) {
           $(".sidebar__next").off("click");
         });
       }
-
     });
   });
 }
@@ -45,43 +44,91 @@ function printEpisodes(episodes) {
     $(".sidebar__list").append(`
         <li class="sidebar__list__item" data-episode-id="${episode.id}">
             <p class="list__item__title">${episode.name}</p>
-            <div class="list__item__div">
-                <p>${episode.air_date}</p>
-                <p>${episode.episode}</p>
-            </div>
+            <p>${episode.episode}</p>
         </li>`);
-  });
 
+        // $(".sidebar__list").append(`
+        // <li class="sidebar__list__item" data-episode-id="${episode.id}">
+        //     <p class="list__item__title">${episode.name}</p>
+        //     <div class="list__item__div">
+        //         <p>${episode.air_date}</p>
+        //         <p>${episode.episode}</p>
+        //     </div>
+        // </li>`);
+
+  });
 }
 
 //Takes the result of all episodes data as parameter
 function getEpisodeCharacters(episodes) {
-  //When clicking each list item, take its episode id, create a new '.container_main'
+  //When clicking each list item, take its episode id, create a new '.container_cards'
   //remove its content and append it to the 'main' section
-  $('.sidebar__list__item').on('click', function(e){
-    var episodeId = e.target.closest('li').dataset.episodeId;
-    var containerMain = $(`<div class="container_main"></div>`);
-    $('.container_main').remove();
-    $('main').append(containerMain);
+  $(".sidebar__list__item").on("click", function (e) {
+    var episodeId = e.target.closest("li").dataset.episodeId;
+    var containerCards = $(`<div class="container_cards"></div>`);
+
+    $(".container_cards").remove();
+    $(".container_main").append(containerCards);
+
+    
+
 
     //Iterate over the episodes array, and for each episode compare its id with the target id
     episodes.forEach((episode) => {
       if (episode.id == episodeId) {
+
+        $(".episode_preview").remove();
+        $(".container_episode").append(`
+          <div class="episode_preview">
+            <h1>${episode.name}</h1>
+            <div>
+              <p>${episode.air_date}</p>
+              <p>${episode.episode}</p>
+            </div> 
+          </div>
+        `)
+
         //If the ids match, iterate over that episode's characters array
         //and make an API call for each character
         episode.characters.forEach((character) => {
           axios.get(character).then((response) => {
-            printEpisodeCharacters(response.data)
-          })
-        })
+            printEpisodeCharacters(response.data);
+            
+          });
+        });
       }
-    })
-  })
+    });
+  });
 }
 
 //Take the data of each character's individual API call as parameter
 function printEpisodeCharacters(character) {
-  console.log(character.name)
-  $('.container_main').append(`
-  <p>${character.name}</p>`)
+  console.log(character.name);
+  $(".container_cards").append(`
+  <div class="character_card__container">
+    <div class="character_card">
+      <div class="character_card__front">
+        <img src="${character.image}" alt="${character.name}" />
+      </div>
+      <div class="character_card__back">
+        <p class="character_name">${character.name}</p>
+        <div>
+          <p>Status: ${character.status}</p>
+          <p>Gender: ${character.gender}</p>
+          <p>Origin: ${character.origin.name}</p>
+        </div>
+        <button data-location-url="${character.location.url}">Origin location</button>
+      </div>
+    </div>
+  </div>
+  `);
 }
+
+$(document).ready(function () {
+  const containerMain = $(`<div class="container_main"></div>`)
+  const episodeInfo = $(`<div class="container_episode"></div>`);
+  $('main').append(containerMain);
+  $(containerMain).append(episodeInfo);
+  $(episodeInfo).append(`<h1 class="episode_preview">Select an episode to show its characters</h1>`)
+
+});
